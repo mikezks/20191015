bbc3ecd61f86ea9a7099e81c4971cd9c0d07362d
mikezks <michael.zikes@intauria.com>
2019-10-17 15:10:20 +0200

    Unit-Testing

--- src/app/flight-booking/flight-search/flight-search.component.spec.ts
+++ src/app/flight-booking/flight-search/flight-search.component.spec.ts
@@ -1,25 +1,65 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { FlightSearchComponent } from './flight-search.component';
+import { FlightBookingModule } from '../flight-booking.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '../../shared/shared.module';
+import { By } from '@angular/platform-browser';
+import { format } from 'util';
 
-describe('FlightSearchComponent', () => {
+describe('Unit-Test: FlightSearchComponent', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FlightSearchComponent ]
+      declarations: [],
+      imports: [
+        FlightBookingModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        SharedModule
+      ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(FlightSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  }));
+
+  it('should not have any flights loaded initially', () => {
+    expect(component.flights.length).toBe(0);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should have a disabled search button w/o params', fakeAsync(() => {
+    tick();
+
+    const from = fixture
+      .debugElement
+      .query(By.css('input[name=from]'))
+      .nativeElement;
+
+    from.value = '';
+    from.dispatchEvent(new Event('input'));
+    
+    const to = fixture
+      .debugElement
+      .query(By.css('input[name=to]'))
+      .nativeElement;
+
+    to.value = '';
+    to.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+    tick();
+
+    const disabled = fixture
+      .debugElement
+      .query(By.css('button'))
+      .properties['disabled'];
+
+    expect(disabled).toBeTruthy();
+  }));
 });
